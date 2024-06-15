69ee2f9b231578cc5dcae114d314aad127b26dff
Ian Eykamp <ian@eykamp.com>
2024-06-16 00:38:34 +0200

    display tick labels with si prefix format

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <Chart :points="currents" xAxisLabel="Time" yAxisLabel="Value" xUnit="s" yUnit="mV"/>
-    <Chart :points="saturationLevels" xAxisLabel="Time" yAxisLabel="Value" xUnit="s" yUnit="mV"/>
+    <Chart :points="currents" xAxisLabel="Vgs" yAxisLabel="Current" xUnit="V" yUnit="A" v-bind:cornerToCornerGraph="true"/>
+    <Chart :points="saturationLevels" xAxisLabel="Vds" yAxisLabel="% of Max. Current" xUnit="V" yUnit="%" v-bind:cornerToCornerGraph="true"/>
     <Diagram />
   </div>
 </template>
@@ -12,7 +12,6 @@ import Diagram from './components/Diagram.vue'
 
 import { ekvNmos } from './functions/ekvModel'
 import { unit } from 'mathjs'
-
 
 function linspace(start: number, end: number, num: number): number[] {
   if (num <= 0) {
@@ -51,7 +50,7 @@ for (let i = 0; i < current.length; i++) {
 // saturation level
 const saturationLevels: Point[] = []
 const Vds = linspace(0, 1, 1000);
-const saturationLevel = Vds.map(n => ekvNmos(unit(1, 'V'), unit(0, 'V'), unit(n, 'V')).saturationLevel)
+const saturationLevel = Vds.map(n => ekvNmos(unit(1, 'V'), unit(0, 'V'), unit(n, 'V')).saturationLevel * 100) // to percent
 for (let i = 0; i < current.length; i++) {
   saturationLevels.push({
     x: Vgs[i],
---
+++ src/functions/toSiPrefix.ts
@@ -0,0 +1,35 @@
+export const toSiPrefix = (n: number, unit: string, sigFigs: number = 3) => {
+    if (n == 0) {
+        return '0' + unit
+    }
+    const sign = n >= 0 ? '' : '-'
+    const magnitude = Math.abs(n)
+
+    const prefixes: { [key: string]: string } = {
+    '30': 'Q',
+    '27': 'R',
+    '24': 'Y',
+    '21': 'Z',
+    '18': 'E',
+    '15': 'P',
+    '12': 'T',
+    '9': 'G',
+    '6': 'M',
+    '3': 'k',
+    '0': '',
+    '-3': 'm',
+    '-6': 'µ',
+    '-9': 'n',
+    '-12': 'p',
+    '-15': 'f',
+    '-18': 'a',
+    '-21': 'z',
+    '-24': 'y',
+    '-27': 'r',
+    '-30': 'q',
+    }
+    const exponent = Math.floor(Math.log10(magnitude) / 3) * 3
+    const prefix = prefixes[exponent.toString()]
+    const mantissa = parseFloat((magnitude / (10 ** exponent)).toPrecision(sigFigs))
+    return sign + mantissa + prefix + unit
+}
