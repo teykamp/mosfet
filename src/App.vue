2b0d3f3ef263df9d55f8e7e7b8c5cf5af8ac0121
Thomas Eykamp <embergameslp@gmail.com>
2024-08-08 12:33:30 -0400

    add switch toggle

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <Diagram />
+    <Switch v-model="test" :label-up="'HIhihi'" :label-down="'hohoHOho'"/>
+    {{ test }}
   </div>
 </template>
 
 <script setup lang="ts">
 import Diagram from './components/Diagram.vue'
+import Switch from './components/Switch.vue'
+import { ref } from 'vue'
+
+const test = ref(false)
 </script>
---
+++ src/components/Switch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div @click="toggle" style="cursor: pointer;">
+    <div style="display: flex; margin-bottom: -30px;">
+      <div style="width: 10px; height: 0; margin-top: 28px; border: 1px solid black; margin-left: 38px;"></div>
+      <p>{{ props.labelUp }}</p>
+    </div>
+    <div style="margin-top: 20px;  margin-bottom: 20px; display: flex;">
+      <div style="width: 10px; border: 1px solid black;"></div>
+      <div
+        :style="`width: 30px; border: 1px solid black; transform: rotate(${props.modelValue ? '35deg' : '-35deg'}); transform-origin: 0% 50%`">
+      </div>
+    </div>
+    <div style="display: flex; margin-top: -30px;">
+      <div style="width: 10px; height: 0; margin-top: 26px; border: 1px solid black; margin-left: 38px;"></div>
+      <p>{{ props.labelDown }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+const props = defineProps<{ 
+  modelValue: boolean,
+  labelUp: string,
+  labelDown: string
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: boolean): void
+}>()
+
+const toggle = () => {
+  emit('update:modelValue', !props.modelValue)
+}
+</script>
+
+<style scoped>
+
+</style>
