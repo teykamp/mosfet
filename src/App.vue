0667934fa437c9742a51490a053d3c1965c414b7
Thomas Eykamp <embergameslp@gmail.com>
2024-08-13 22:26:49 -0400

    move block middle click code

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,11 @@
 <script setup lang="ts">
 import Diagram from './components/Diagram.vue'
 // import Switch from './components/Switch.vue'
+
+document.body.onmousedown = function(e) {
+    if(e.button == 1) {
+        e.preventDefault();
+        return false;
+    }
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-document.body.onmousedown = function(e) {
-    if(e.button == 1) {
-        e.preventDefault();
-        return false;
-    }
-}
-
 createApp(App).mount('#app')
