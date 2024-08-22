e55e9bb27e81bc35530bb1d5f840ee4e1cdb107a
Thomas Eykamp <embergameslp@gmail.com>
2024-08-21 20:37:58 -0400

    add box shadow

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="props.data.title !== ''">
-    <div :style="`background-color: #FF7F7F; border-radius: 10px; border: 2px solid red; position: absolute; max-width: 400px; padding: 20px; ${props.data.location ? props.data.location.y : 'top: 45vh'}; ${props.data.location ? props.data.location.x : 'left: calc(50vw - 200px'});`">
+    <div :style="`background-color: #FF7F7F; border-radius: 10px; border: 2px solid red; position: absolute; max-width: 400px; padding: 20px; ${props.data.location ? props.data.location.y : 'top: 45vh'}; ${props.data.location ? props.data.location.x : 'left: calc(50vw - 200px'}); box-shadow: rgba(0, 0, 0, 0.4) 0px 8px 24px;`">
       <h3 style="color: white;">{{ props.data.title }}</h3>
       <p style="color: white;">{{ props.data.text }}</p>
       <div style="display: flex; justify-content: space-around;">
